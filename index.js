f9642eda7f0d1ac7bb77c6d69ee59110f84547ea
nicolascossi <77031613+nicolascossi@users.noreply.github.com>
2023-03-15 11:22:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+// ANIMACION SIDEBAR //
+
+const body = document.querySelector('body'),
+sidebar = body.querySelector('nav'),
+toggle = body.querySelector(".toggle"),
+searchBtn = body.querySelector(".search-box"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+toggle.addEventListener("click" , () =>{
+sidebar.classList.toggle("close");
+})
+
+// MODALES ABRIR //
+
+const newInvoice = document.querySelector ("#NewInvoiceModal")
+
+const modal = document.querySelector ("#modalBackground")
+newInvoice.addEventListener ("click", ( ) => {
+    modal.classList.add ("show")
+    console.log("Hola")
+})
+
+
+modal.addEventListener ("click",(e) => {
+    if (e.target === modal) modal.classList.remove ("show")
+})
+
+const statuInvoice = document.querySelector ("#NewInvoiceModal2")
+
+const modal2 = document.querySelector ("#modalBackground2")
+statuInvoice.addEventListener ("click", ( ) => {
+    modal2.classList.add ("show2")
+    console.log("Hola")
+})
+
+
+modal2.addEventListener ("click",(e) => {
+    if (e.target === modal2) modal2.classList.remove ("show2")
+})
+
+
+
+
+
+
